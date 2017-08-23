c6e35e3cb4f3f402c29e005bbc362ccc5c38071b
Vikram <vkommaraju@salesforce.com>
2017-08-22 22:44:36 -0700

    first commit

--- src/main/java/spring/react/starter/configuration/Application.java
+++ src/main/java/spring/react/starter/configuration/Application.java
@@ -1,12 +1,13 @@
-package spring.jpa.rest.configuration;
+package spring.react.starter.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ComponentScan;
 
-@EntityScan("spring.jpa.rest.model")
-@EnableJpaRepositories("spring.jpa.rest.repository")
+//@EntityScan("spring.jpa.rest.model")
+//@EnableJpaRepositories("spring.jpa.rest.repository")
+@ComponentScan("spring.react.starter.controller")
 @SpringBootApplication
 public class Application {
 
